285b6de028c8372ec7b459ebea0daa078a4f2026
Syed Shanto <syed.shanto@dal.ca>
2024-04-20 07:55:56 -0300

    added a copy function to copy all my scripts to home/username/.local/bin

---
+++ bash/cp_shtobin.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+cp_sh_to_bin () {
+        if [ $# -ne 1 ]; then
+                echo "Usage: $0 folder_location"
+                exit 1
+        fi
+
+        if [ ! -d ~/.local/bin ]; then
+                mkdir -p ~/.local/bin
+        fi
+
+        for file in "$1"/*.sh; do
+                if [ -f "$file" ]; then
+                        fname=$(basename "$file" .sh)
+                        cp "$file" ~/.local/bin/"$fname"
+                        chmod +x ~/.local/bin/"$filename"
+                fi
+        done
+        echo "All *.sh files copied to ~/.local/bin"
+}
+
+cp_sh_to_bin "$#"
